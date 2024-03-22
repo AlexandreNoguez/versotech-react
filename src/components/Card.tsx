b623049967dbd6c7df84d2318caa5d242b3889dc
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-03-22 20:27:03 -0300

    fix: added key pokemon map

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,7 @@ interface CardI {
 function Card({ pokemons, isNew }: CardI) {
     return (
         pokemons.map((pokemon: PokemonI) => (
-            <Link to={`/pokemon/${pokemon.name}`}>
+            <Link key={pokemon.name} to={`/pokemon/${pokemon.name}`}>
 
                 <li className="border border-white px-4 py-2 w-36 rounded-lg hover:bg-slate-400 hover:text-slate-900 hover:-translate-y-1 transition-all " key={pokemon.name}>
                     {pokemon.name} {isNew ? "NOVO" : null}
