5731cb51ad2521927e2583464159287bfe2d98af
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-03-22 14:48:14 -0300

    feat: added link to specific pokemon

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 interface PokemonI {
     name: string;
     url: string;
@@ -11,9 +13,12 @@ interface CardI {
 function Card({ pokemons, isNew }: CardI) {
     return (
         pokemons.map((pokemon: PokemonI) => (
-            <li className="border border-white px-4 py-2 w-36 rounded-lg hover:bg-slate-400 hover:text-slate-900 hover:-translate-y-1 transition-all " key={pokemon.name}>
-                {pokemon.name} {isNew ? "NOVO" : null}
-            </li>
+            <Link to={`/pokemon/${pokemon.name}`}>
+
+                <li className="border border-white px-4 py-2 w-36 rounded-lg hover:bg-slate-400 hover:text-slate-900 hover:-translate-y-1 transition-all " key={pokemon.name}>
+                    {pokemon.name} {isNew ? "NOVO" : null}
+                </li>
+            </Link>
         )))
 }
 
