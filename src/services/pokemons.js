004c75028683a79d109eb702e799316eb8dcfbda
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-03-22 13:33:51 -0300

    feat: fetch pokeapi service

---
+++ src/services/pokemons.js
@@ -0,0 +1,33 @@
+import { PokemonActions } from "../redux/pokemons/actionTypes";
+import Api from "./axiosConfig";
+
+export async function fetchPokemon(dispatch) {
+    dispatch({
+        type: PokemonActions.REQUEST,
+        payload: true
+    })
+
+    const response = await Api.get('/pokemon');
+
+    console.log(response);
+
+    try {
+        if (response) {
+            /**
+             * Simulando um fetch um pouco mais demorado
+             */
+            setTimeout(() => {
+                dispatch({
+                    type: PokemonActions.SUCCESS,
+                    payload: response.data.results
+                })
+            }, 2000)
+        }
+    } catch (err) {
+        dispatch({
+            type: PokemonActions.FAILURE,
+            payload: `Falha ao consultar API ${err.message}`
+        })
+    }
+
+}
