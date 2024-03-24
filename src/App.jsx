dee0a7376be2641451cf93193def92c4537f37b5
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-03-24 17:32:05 -0300

    fix: making mobile extra small view better

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import Header from "./components/Header"
 import AppRoutes from "./routes/AppRoutes"
 
 import 'react-toastify/dist/ReactToastify.css';
+import useWindowSize from "./hooks/SizeObserver";
 
 function App() {
-
+  const { width } = useWindowSize();
   return (
     <div className="flex flex-col h-screen">
 
       <Header />
-      <div className="px-8 py-4">
+      <div className={`${width < 480 ? "px-2 py-4" : "px-8 py-4"}`}>
         <AppRoutes />
       </div>
       <ToastContainer />
