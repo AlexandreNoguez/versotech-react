f1c65ec0e9902ea6ecb3494fd1a5f634027a83d0
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-03-24 17:34:26 -0300

    feat: added pokémon details and charts to statistics

---
+++ src/components/PokeCharts.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import ReactApexChart from "react-apexcharts";
+
+PokeCharts.propTypes = {
+    stats: PropTypes.object
+};
+
+function PokeCharts({ stats: series }) {
+
+    let options = {
+        yaxis: {
+            show: false,
+        },
+        dataLabels: {
+            enabled: true,
+            background: {
+                enabled: true,
+                borderRadius: 2,
+            }
+        },
+        labels: ['HP', 'Ataque', 'Defesa', 'Ataque Especial', 'Defesa Especial', 'Velocidade'],
+        plotOptions: {
+            radar: {
+                polygons: {
+                    strokeColor: '#e8e8e8',
+                    fill: {
+                        colors: ['#f8f8f8', '#fff']
+                    }
+                }
+            }
+        }
+    }
+
+    return (
+        series ? (
+            <div id="chart">
+                <ReactApexChart
+                    options={options}
+                    series={series}
+                    type="radar"
+                    height={250}
+                    width={300}
+                />
+            </div>
+        ) : null
+    )
+}
+
+export default PokeCharts;
--- src/pages/PokeDetails.jsx
+++ src/pages/PokeDetails.jsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { fetchPokemonByName } from '../services/pokemons'
+import Loading from "../components/Loading";
+import PokeCharts from "../components/PokeCharts";
 
 function PokeDetails() {
     const { name } = useParams();
     const [pokemonDetails, setPokemonDetails] = useState();
     const [isLoading, setIsLoading] = useState(true);
+    const [stats, setStats] = useState();
 
     async function getPokemon() {
         try {
             const pokemon = await fetchPokemonByName(name);
-            setPokemonDetails(pokemon);
+            setPokemonDetails(pokemon.data);
+            setStats(pokemon.stats);
             setIsLoading(false);
         } catch (error) {
             console.error(error);
@@ -22,12 +26,41 @@ function PokeDetails() {
     useEffect(() => {
         getPokemon();
         // eslint-disable-next-line
-    }, [])
+    }, []);
 
     return (
-        <h1>
-            {isLoading ? "Carregando..." : (pokemonDetails && JSON.stringify(pokemonDetails))} {pokemonDetails}
-        </h1>
+        isLoading
+            ? (<Loading />)
+            : (pokemonDetails ?
+                <div className="flex flex-col">
+                    <div className="flex flex-col gap-2 mb-4 w-[300px] mx-auto justify-center items-center bg-slate-500 rounded-lg py-2">
+                        <h1>{name}</h1>
+                        <img src={pokemonDetails.sprites.front_default} alt="" />
+                        <p>Nº {pokemonDetails.order}</p>
+                        <div>
+                            <p>Tipo:</p>
+                            {pokemonDetails.types.map((type, index) => (
+                                <p key={index}>
+                                    {type.type.name}
+                                </p>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-2 mb-4 w-[300px] mx-auto justify-center items-center bg-slate-500 rounded-lg py-2">
+                        <p>Habilidades</p>
+                        {pokemonDetails.abilities.map((ability, index) => (
+                            <p key={index}>{ability.ability.name}</p>
+                        ))}
+                    </div>
+                    <div className="flex flex-col gap-2 mb-4 w-[300px] mx-auto justify-center items-center bg-slate-500 rounded-lg py-2">
+                        <p>estatísticas</p>
+                        {pokemonDetails?.stats ?
+                            <PokeCharts stats={stats} />
+                            : null}
+                    </div>
+                </div>
+                : null
+            )
     )
 }
 
