132054c0802deae718ba82b7ec477b7dc6f5da97
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-03-26 11:38:49 -0300

    feat: added redux action

--- src/redux/pokemons/actions.js
+++ src/redux/pokemons/actions.js
@@ -1,5 +1,10 @@
 import PokemonActionsTypes from "./actionTypes";
 
+export const fetchLoadingPokemons = (payload) => ({
+    type: PokemonActionsTypes.REQUEST,
+    payload
+})
+
 export const fetchSinglePokemon = (payload) => ({
     type: PokemonActionsTypes.SUCCESS,
     payload
